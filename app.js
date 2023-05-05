feb385fe99c5eed8d4342667d1740927fe37885d
Micah Asowata <andrewgary963@gmail.com>
2023-05-05 09:30:23 +0100

    Added dynamic page titles

--- app.js
+++ app.js
@@ -6,17 +6,17 @@ app.listen(port);
 app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
-  res.render("index");
+  res.render("index", { title: "Home" });
 });
 
 app.get("/about", (req, res) => {
-  res.render("about");
+  res.render("about", { title: "About" });
 });
 
 app.get("/blog/create", (req, res) => {
-  res.render("create");
+  res.render("create", { title: "Create" });
 });
 
 app.use((req, res) => {
-  res.status(404).render("404");
+  res.status(404).render("404", { title: "Not Found" });
 });
