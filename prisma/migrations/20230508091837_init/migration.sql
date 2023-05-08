023092346aa6a515b578d7b28a6efe4d91127ef3
Micah Asowata <andrewgary963@gmail.com>
2023-05-08 10:30:43 +0100

    Successfully created db tables with prisma

---
+++ prisma/migrations/20230508091837_init/migration.sql
@@ -0,0 +1,11 @@
+-- CreateTable
+CREATE TABLE `Post` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+    `title` VARCHAR(100) NOT NULL,
+    `snippet` VARCHAR(300) NOT NULL,
+    `content` VARCHAR(191) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
