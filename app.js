e3cb7f7384f1e23e510f4cdcfe539848b868a62d
Micah Asowata <andrewgary963@gmail.com>
2023-05-10 06:27:17 +0100

    Added all the necessary routes

--- app.js
+++ app.js
@@ -22,71 +22,33 @@ main()
   });
 
 app.set("view engine", "ejs");
+
 app.use(morgan("dev"));
 app.use(express.static("static"));
-app.get("/add-blog", (req, res) => {
-  const postTitle = "hello test";
-  const postSnippet = "hello new test snippet";
-  const postBody = "hello new test body";
 
-  createPost(postTitle, postSnippet, postBody)
-    .then(() => {
-      res.send(
-        JSON.stringify({
-          message: "created",
-        })
-      );
-    })
-    .catch((e) => {
-      console.error(e);
-    });
-});
-
-app.get("/view-blog", (req, res) => {
-  const ressPost = getPostByID(1);
-
-  ressPost.then((post) => res.send(post)).catch((e) => console.error(e));
-  // console.log(ressPost + "Hello");
-});
-
-app.get("/delete-post", (req, res) => {
-  const id = 3;
-  const delPost = deletePost(id);
-
-  delPost.then((post) => res.send(post)).catch((e) => console.error(e));
-});
 app.get("/", (req, res) => {
-  // const ress = [
-  //   {
-  //     title: "Example 1",
-  //     snippet: "This is an example snippet.",
-  //     body: "This is an example body. It can contain more detailed information about the example.",
-  //   },
-  //   {
-  //     title: "Example 2",
-  //     snippet: "Another example snippet.",
-  //     body: "This is a different example body. It could contain information about how the example differs from the first one.",
-  //   },
-  //   {
-  //     title: "Example 3",
-  //     snippet: "Yet another example snippet.",
-  //     body: "And here's another example body. This one could provide information about a completely different aspect of the object.",
-  //   },
-  // ];
-
-  const ress = getAllPosts()
-    .then((ress) => {
-      res.render("index", { title: "Home", ress: ress });
-    })
-    .catch((e) => console.error(e));
+  res.redirect("/blogs");
 });
-
 app.get("/about", (req, res) => {
   res.render("about", { title: "About" });
 });
 
-app.get("/blog/create", (req, res) => {
+app.get("/blogs", (req, res) => {
+  res.send("All blogs...");
+});
+app.get("/blog/new", (req, res) => {
   res.render("create", { title: "Create" });
+});
+
+app.get("/blog/view", (req, res) => {
+  res.send("Viewing blog...");
+});
+app.post("/blog/create", (req, res) => {
+  res.send("Creating blog...");
+});
+
+app.get("/blog/delete", (req, res) => {
+  res.send("Deleting blog...");
 });
 
 app.use((req, res) => {
